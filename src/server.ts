26aa7aa2d20e717e0b6bfb679d45f770da7cc278
hrittik-chatterjee <hrittik0chatterjee@gmail.com>
2025-06-22 20:11:48 +0600

    fixed uri problem

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,6 @@ const PORT = 5000;
 
 async function main() {
   try {
-    // await mongoose.connect(
-    //   "mongodb+srv://hrittik:IaaD9NfGIeeJ2v1A@cluster0.tkzq2fj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    // );
-
     await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("Connected to MongoDB Using Mongoose!!");
     server = app.listen(PORT, () => {
